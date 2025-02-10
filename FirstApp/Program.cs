ae7bf0dc3550dc77923eb397ff5a1e23cc3b3b80
Need4r5peed <windrayn@yandex.ru>
2025-02-11 01:27:05 +0300

    Переменные, константы, литеры.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 namespace FirstApp
 {
@@ -8,10 +9,23 @@ namespace FirstApp
         {
             string myName;
             myName = "Jane";
+            const string MyName = "Jane";
             string mySecondName = "Li";
 
             Console.WriteLine(myName);
             Console.WriteLine(mySecondName);
+            Console.WriteLine("Привет, \n мир");
+            Console.WriteLine(MyName);
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23');
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
 
             Console.ReadKey();
         }
