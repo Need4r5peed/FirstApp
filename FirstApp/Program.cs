456ebbc354e4363a101cd278bc6a3863cff5aa4e
Need4r5peed <windrayn@yandex.ru>
2025-02-11 23:54:53 +0300

    Пример который пригодится.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,28 +7,15 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Jane";
-            const string MyName = "Jane";
-            string mySecondName = "Li";
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
 
-            Console.WriteLine(myName);
-            Console.WriteLine(mySecondName);
-            Console.WriteLine("Привет, \n мир");
-            Console.WriteLine(MyName);
-            Console.WriteLine("\t Привет Мир");
-            Console.WriteLine("\t Мне 27 лет");
-            Console.WriteLine("\t My name is \n {0}", MyName);
-            Console.WriteLine('\u0040');
-            Console.WriteLine('\x23');
-            Console.WriteLine(true);
-            Console.WriteLine(false);
-            Console.WriteLine(0x0A);
-            Console.WriteLine(0b11);
-            Console.WriteLine(5.5);
-            Console.WriteLine(5.5);
-            Console.WriteLine("Завершение юнита 3.2. Переменные, константы, литералы");
-            Console.ReadKey();
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
         }
     }
 }
