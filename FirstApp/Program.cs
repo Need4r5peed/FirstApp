056e9df63c31b4a11c70f549de570229d0ddd6df
Need4r5peed <windrayn@yandex.ru>
2025-02-12 23:44:33 +0300

    Завершение юнита 3.6. Основные операции C#

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,43 +7,13 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string MyName = "Jane";
-            byte MyAge = 27;
-            bool HaveIApet = true;
-            double MyShoeSize = 37.5;
+            double result1 = 5.0 / 2;
+            double result2 = 5.0 % 2;
+            Console.WriteLine("5.0 / 2 = {0}", result1);
+            Console.WriteLine("5.0 % 2.0 = {0}", result2);
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("MyAge " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
-            Console.WriteLine("IntMin {0} ", int.MinValue);
-            Console.WriteLine("IntMax {0} ", int.MaxValue);
-
-            DaysOfWeek MyFavoriteDay;
-            MyFavoriteDay = DaysOfWeek.Monday;
-            Console.WriteLine(MyFavoriteDay);
-
-            var name = "Jane";
-            var age = 27;
-            var favcolor = "black";
-            Console.WriteLine("{0}\n\t{1}\n{2}", name, age, favcolor);
-
-        }
-        enum DaysOfWeek : byte
-        {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sanday
-        }
-        enum Semaphore
-        {
-            Red = 100,
-            Yellow = 200,
-            Green = 300
+            double result3 = 10 % 3;
+            Console.WriteLine("10 % 3 = {0}", result3);
         }
     }
 }
