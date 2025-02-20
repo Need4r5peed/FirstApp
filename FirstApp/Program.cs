cdc1c83871f7ddbf1ec48cc6588827b6a2d6172c
Need4r5peed <windrayn@yandex.ru>
2025-02-20 20:00:43 +0300

    Модуль 5 юнит 1/9   Введение задание №1 кортеж с блюдами

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,41 +9,18 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            (string Name, string LastName, string Login, int LoginLength, bool ExistencePet, double Age, string[] FavColors) User;
+            (string Name, string[] Dishes) User;
 
             for (int x = 1; x <= 3; x++)
             {
                 Console.WriteLine("Введите имя");
                 User.Name = Console.ReadLine();
 
-                Console.WriteLine("Введите фамилию");
-                User.LastName = Console.ReadLine();
-
-                Console.WriteLine("Введите логин");
-                User.Login = Console.ReadLine();
-
-                //Длинна пароля:
-                User.LoginLength = User.Login.Length;
-
-                Console.WriteLine("Есть ли у вас животные? Да или Нет");
-                var AnswerAboutPet = Console.ReadLine();
-                if (AnswerAboutPet == "Да")
+                User.Dishes = new string[5];
+                Console.WriteLine("Введите три любимых блюда:");
+                for (int i = 0; i < User.Dishes.Length; i++)
                 {
-                    User.ExistencePet = true;
-                }
-                else
-                {
-                    User.ExistencePet = false;
-                }
-
-                Console.WriteLine("Введите возраст пользователя");
-                User.Age = double.Parse(Console.ReadLine());
-
-                User.FavColors = new string[3];
-                Console.WriteLine("Введите три любимых цвета пользователя");
-                for (int i = 0; i < User.FavColors.Length; i++)
-                {
-                    User.FavColors[i] = Console.ReadLine();
+                    User.Dishes[i] = Console.ReadLine();
                 }
             }
         }
