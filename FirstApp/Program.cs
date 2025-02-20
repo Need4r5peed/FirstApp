4398f943f5c1573a2e33f0db03640f6321751961
Need4r5peed <windrayn@yandex.ru>
2025-02-20 17:57:24 +0300

    Задание №6 Ввод анкеты для 3 пользователей.

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -11,39 +11,41 @@ namespace FirstApp
         {
             (string Name, string LastName, string Login, int LoginLength, bool ExistencePet, double Age, string[] FavColors) User;
 
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
-
-            Console.WriteLine("Введите фамилию");
-            User.LastName = Console.ReadLine();
-
-            Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-
-            //Длинна пароля:
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-            var AnswerAboutPet = Console.ReadLine();
-            if (AnswerAboutPet == "Да")
+            for (int x = 1; x <= 3; x++)
             {
-                User.ExistencePet = true;
-            }
-            else
-            {
-                User.ExistencePet = false;
-            }
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
 
-            Console.WriteLine("Введите возраст пользователя");
-            User.Age = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите фамилию");
+                User.LastName = Console.ReadLine();
 
-            User.FavColors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
-            for (int i = 0; i < User.FavColors.Length; i++)
-            {
-                User.FavColors[i] = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+
+                //Длинна пароля:
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                var AnswerAboutPet = Console.ReadLine();
+                if (AnswerAboutPet == "Да")
+                {
+                    User.ExistencePet = true;
+                }
+                else
+                {
+                    User.ExistencePet = false;
+                }
+
+                Console.WriteLine("Введите возраст пользователя");
+                User.Age = double.Parse(Console.ReadLine());
+
+                User.FavColors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.FavColors.Length; i++)
+                {
+                    User.FavColors[i] = Console.ReadLine();
+                }
             }
-
         }
 
         enum Day : byte
