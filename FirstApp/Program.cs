31271aed5c9a8b5d0a78a971cf2bca6d7640e6f7
Need4r5peed <windrayn@yandex.ru>
2025-02-26 18:29:44 +0300

    Два параметра: username и age - для метода ShowColor 5.2.7 Методы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,9 +7,9 @@ namespace FirstApp
 {
     internal class Program
     {
-        static string ShowColor()
+        static string ShowColor(string username, int age)
         {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            Console.WriteLine($"{username}({age} лет),{Environment.NewLine}Напишите свой любимый цвет на английском с маленькой буквы");
             var color = Console.ReadLine();
 
             switch (color)
@@ -46,33 +46,43 @@ namespace FirstApp
         public static void Main(string[] args)
         {
 
-            //var (name, age) = ("Евгения", 27);
+            var (name, age) = ("Евгения", 27);
 
-            //Console.WriteLine("Мое имя: {0}", name);
-            //Console.WriteLine("Мой возраст: {0}", age);
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите возрас с цифрами:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ваше имя: {0}", name);
+            Console.WriteLine("Ваш возраст: {0}", age);
+
+            ///
+
+            //(string name, int age) anketa;
 
             //Console.Write("Введите имя: ");
-            //name = Console.ReadLine();
-            //Console.Write("Введите возрас с цифрами:");
-            //age = Convert.ToInt32(Console.ReadLine());
+            //anketa.name = Console.ReadLine();
+            //Console.Write("Введите возраст с цифрами: ");
+            //anketa.age = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("Ваше имя: {0}", name);
-            //Console.WriteLine("Ваш возраст: {0}", age);
+            //Console.WriteLine("Ваше имя: {0}", anketa.name);
+            //Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
-            //ShowColor();
+            var favcolors = new string[3];
 
-            //var favcolors = new string[3];
+            for (int i = 0; i < favcolors.Length; i++)
+            {
+                favcolors[i] = ShowColor(name, age);
+            }
 
-            //for (int i = 0; i < favcolors.Length; i++)
-            //{
-            //    favcolors[i] = ShowColor();
-            //}
-
-            //Console.WriteLine("Ваши любимые цвета:");
-            //foreach (var color in favcolors)
-            //{
-            //    Console.WriteLine(color);
-            //}
+            Console.WriteLine("Ваши любимые цвета:");
+            foreach (var color in favcolors)
+            {
+                Console.WriteLine(color);
+            }
 
             static int[] GetArrayFromConsole()
             {
@@ -112,7 +122,7 @@ namespace FirstApp
                 return result;
             }
 
-            GetArrayFromConsole();
+            //GetArrayFromConsole();
         }    
     }
 }
