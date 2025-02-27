5b26765f695df185ea4e43db37f0060fa5c64d82
Need4r5peed <windrayn@yandex.ru>
2025-02-27 07:44:37 +0300

    Разделение метода GetArrayFromConsole на два. Задание 5.2.8 Методы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Threading.Channels;
 using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
@@ -46,18 +47,18 @@ namespace FirstApp
         public static void Main(string[] args)
         {
 
-            var (name, age) = ("Евгения", 27);
+            //var (name, age) = ("Евгения", 27);
 
-            Console.WriteLine("Мое имя: {0}", name);
-            Console.WriteLine("Мой возраст: {0}", age);
+            //Console.WriteLine("Мое имя: {0}", name);
+            //Console.WriteLine("Мой возраст: {0}", age);
 
-            Console.Write("Введите имя: ");
-            name = Console.ReadLine();
-            Console.Write("Введите возрас с цифрами:");
-            age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите имя: ");
+            //name = Console.ReadLine();
+            //Console.Write("Введите возрас с цифрами:");
+            //age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Ваше имя: {0}", name);
-            Console.WriteLine("Ваш возраст: {0}", age);
+            //Console.WriteLine("Ваше имя: {0}", name);
+            //Console.WriteLine("Ваш возраст: {0}", age);
 
             ///
 
@@ -71,20 +72,20 @@ namespace FirstApp
             //Console.WriteLine("Ваше имя: {0}", anketa.name);
             //Console.WriteLine("Ваш возраст: {0}", anketa.age);
 
-            var favcolors = new string[3];
+            //var favcolors = new string[3];
 
-            for (int i = 0; i < favcolors.Length; i++)
-            {
-                favcolors[i] = ShowColor(name, age);
-            }
+            //for (int i = 0; i < favcolors.Length; i++)
+            //{
+            //    favcolors[i] = ShowColor(name, age);
+            //}
 
-            Console.WriteLine("Ваши любимые цвета:");
-            foreach (var color in favcolors)
-            {
-                Console.WriteLine(color);
-            }
+            //Console.WriteLine("Ваши любимые цвета:");
+            //foreach (var color in favcolors)
+            //{
+            //    Console.WriteLine(color);
+            //}
 
-            static int[] GetArrayFromConsole()
+            static int[] GetArrayFromConsole() // Добавление по одному элементу в массив, вместимостью в 5 интовых элементов, сортировка элементов массива по возрастанию и вывод массива в отсортированном виде на консоль
             {
                 var result = new int[5];
 
@@ -122,7 +123,49 @@ namespace FirstApp
                 return result;
             }
 
-            //GetArrayFromConsole();
+            GetArrayFromConsole();
+            static int[] GetArrayFromConsoleOnly() // должен читать введенные с клавиатуры массивы и возвращать их
+            {
+                var result = new int[5];
+
+                for (int i = 0; i < result.Length; i++)
+                {
+                    Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                    result[i] = int.Parse(Console.ReadLine());
+                }
+
+                return result;
+            }
+
+            static int[] SortArray(int[] result) // должен принимать параметром массив array типа данных int, сортировать его и возвращать
+            {
+                int w = 0;
+                while (w != -1 * (result.Length - 1))
+                {
+                    w = 0;
+                    for (int i = 0; i <= result.Length - 2; i++)
+                    {
+                        int a = result[i];
+                        int b = result[i + 1];
+                        if (result[i] > result[i + 1])
+                        {
+                            result[i] = b;
+                            result[i + 1] = a;
+                            w++;
+                        }
+                        else
+                        {
+                            w--;
+                        }
+                    }
+                }
+                //foreach (var number in result)
+                //{
+                //    Console.Write(number + " ");
+                //}
+
+                return result;
+            }
         }    
     }
 }
