70d8ea6b29b75205b2ab7bfddb4de02e3542ecb5
Need4r5peed <windrayn@yandex.ru>
2025-03-12 18:52:11 +0300

    Ренализовать конструктор для Obj Задание 7.1.6 Продолжение ООП

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -55,6 +55,21 @@ namespace FirstApp
     {
         public string nameRootVegetablesVariety;
     }
+    class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName, int objLength, int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
 
     class Program
     {
