f51bf8db34b85ea3416d915b3a33b8f1c79e88f5
Need4r5peed <windrayn@yandex.ru>
2025-03-18 18:33:49 +0300

    Индексатор для класса IndexingClass Задание 7.2.14 Полиморфизм

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -147,6 +147,28 @@ namespace FirstApp
         }
     }
 
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int this[int index]
+        { 
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
