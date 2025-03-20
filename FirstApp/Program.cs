8d89a0b9cdc4d9a21c416e4fa5745cfc00d1b98f
Need4r5peed <windrayn@yandex.ru>
2025-03-20 18:20:49 +0300

    Реализовать класс-обобщение Record Задание 7.6.6 Обобщения

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -27,6 +27,13 @@ namespace FirstApp
     
     }
 
+    class Record<T1, T2>
+    {
+        public T1 Id;
+        public T2 Value;
+        public DateTime Date;
+    }
+
     class Program
     {
         static void Main(string[] args)
