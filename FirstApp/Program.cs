2181f653784b81d46718b369ff3c1de34f70cf08
Need4r5peed <windrayn@yandex.ru>
2025-03-20 16:24:59 +0300

    Создать 2 метода расширения: GetNegative() и GetPositive() Задание 7.5.9 Статические классы и элементы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -26,7 +26,34 @@ namespace FirstApp
             Parent = "System.Object";
             DaysInWeek = 7;
             MaxValue = 2000;
+        }
     }
+
+    static class IntExtensions
+    {
+        public static int GetNegative(this int source)
+        {
+            if (source > 0)
+            {
+                return source * (-1);
+            }
+            else
+            {
+                return source;
+            }
+        }
+
+        public static int GetPositive(this int source)
+        {
+            if (source < 0)
+            {
+                return source * (-1);
+            }
+            else 
+            {
+                return source;
+            }
+        }
     }
 
     class Helper
@@ -44,7 +71,16 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            int num1 = 7;
+            int num2 = -13;
+            int num3 = 0;
 
+            Console.WriteLine(num1.GetNegative()); //-7
+            Console.WriteLine(num1.GetPositive()); //7
+            Console.WriteLine(num2.GetNegative()); //-13
+            Console.WriteLine(num2.GetPositive()); //13
+            Console.WriteLine(num3.GetNegative()); //0
+            Console.WriteLine(num3.GetPositive()); //0
         }
 
     }
