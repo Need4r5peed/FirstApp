4ef3f42191aace5a5639fa8b6edefbf7f1f53f87
Need4r5peed <windrayn@yandex.ru>
2025-03-20 17:07:14 +0300

    Создать класс-обощение Car Задание 7.6.2 Обобщения

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -12,75 +12,26 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FirstApp
 {
-    class Obj
+    class Car<T>
     {
-        public string Name;
-        public string Description;
-
-        public static string Parent;
-        public static int DaysInWeek;
-        public static int MaxValue;
-
-        static Obj()
-        {
-            Parent = "System.Object";
-            DaysInWeek = 7;
-            MaxValue = 2000;
-        }
+        public T Engine;
     }
 
-    static class IntExtensions
+    class ElectricEngine
     {
-        public static int GetNegative(this int source)
-        {
-            if (source > 0)
-            {
-                return source * (-1);
-            }
-            else
-            {
-                return source;
-            }
-        }
-
-        public static int GetPositive(this int source)
-        {
-            if (source < 0)
-            {
-                return source * (-1);
-            }
-            else 
-            {
-                return source;
-            }
-        }
+    
     }
 
-    class Helper
-    {
-        public static void Swap(ref int a, ref int b)
-        {
-            int c = b;
-            b = a;
-            a = c;
-
-        }
+    class GasEngine
+    { 
+    
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = 7;
-            int num2 = -13;
-            int num3 = 0;
 
-            Console.WriteLine(num1.GetNegative()); //-7
-            Console.WriteLine(num1.GetPositive()); //7
-            Console.WriteLine(num2.GetNegative()); //-13
-            Console.WriteLine(num2.GetPositive()); //13
-            Console.WriteLine(num3.GetNegative()); //0
-            Console.WriteLine(num3.GetPositive()); //0
         }
 
     }
