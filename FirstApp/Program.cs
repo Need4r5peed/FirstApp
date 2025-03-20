801b14554b9d6db1c5e7c1807c622041481725e6
Need4r5peed <windrayn@yandex.ru>
2025-03-20 18:43:47 +0300

    Добавить в класс Car виртуальный обобщённый метод ChangePart Задание 7.6.7 Обобщения

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -12,17 +12,37 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace FirstApp
 {
-    class Car<T>
+    class Car<T1>
     {
-        public T Engine;
+        public T1 Engine;
+
+        public virtual void ChangePart<T2>(T2 newPart)
+        { 
+        
+        }
     }
 
     class ElectricEngine
     {
-    
+
     }
 
     class GasEngine
+    {
+
+    }
+
+    class Battery
+    { 
+    
+    }
+
+    class Differential
+    { 
+    
+    }
+
+    class Wheel
     { 
     
     }
