8b392e6bc6c16ea726149fc55039b74c9bb01d6d
Need4r5peed <windrayn@yandex.ru>
2025-03-29 19:30:44 +0300

    Использование GetDrives() 8.2. Классы для работы с дисками и директориями

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,58 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics.Metrics;
-using System.Drawing;
-using System.Globalization;
-using System.Net;
-using System.Reflection.Metadata;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Threading.Channels;
-using System.Xml.Linq;
-using static FirstApp.DiskInfo;
-using static System.Net.Mime.MediaTypeNames;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.IO;
 
-namespace FirstApp
+namespace DriveManager
 {
-    public class DiskInfo
-    {
-        public string DiskName { get; }
-        public double DiskTotalSpace { get; }
-        public double DiskFreeSpace { get; }
-
-        public DiskInfo(string name, double totalSpace, double freeSpace)
-        {
-            DiskName = name;
-            DiskTotalSpace = totalSpace;
-            DiskFreeSpace = freeSpace;
-        }
-
-        public class Folder
-        {
-            public List<string> Files { get; set; } = [];
-        }
-
-    }
-
-    public class Folders<TKey>
-        where TKey : notnull
-    {
-        public Dictionary<TKey, Folder> Files { get; set; } = [];
-
-        public void CreatingFolder(TKey nameFolder)
-        {
-            Files.Add(nameFolder, new Folder());
-        }
-    }
-
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
 
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
     }
 }
