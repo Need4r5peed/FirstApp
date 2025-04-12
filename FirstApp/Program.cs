36aba8649857a75c8cb540fab536f4e5a1b2eed1
Need4r5peed <windrayn@yandex.ru>
2025-04-12 03:48:47 +0300

    Сборка консолького решения по примеру Задание 9.3.7 Делегаты

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,11 +7,26 @@ namespace FirstApp9_3
         
         public delegate void DelegateArithmeticOperations(int a, int b);
 
+        delegate void ShowMessageDelegate();
+        delegate int SumDelegate(int a, int b, int c);
+        delegate bool CheckLengthDelegate(string _row);
+
         static void Main(string[] args)
         {
-            DelegateArithmeticOperations delegateArithmeticOperations = MinusNumbers;
-            delegateArithmeticOperations -= PlusNumbers;
-            delegateArithmeticOperations.Invoke(100, 200);
+            //DelegateArithmeticOperations delegateArithmeticOperations = MinusNumbers;
+            //delegateArithmeticOperations -= PlusNumbers;
+            //delegateArithmeticOperations.Invoke(100, 200);
+
+            ShowMessageDelegate showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke();
+
+            SumDelegate sumDelegate = Sum;
+            int result = sumDelegate.Invoke(1, 30, 120);
+            Console.WriteLine(result);
+
+            CheckLengthDelegate checkLengthDelegate = CheckLength;
+            bool status = checkLengthDelegate.Invoke("skill_factory");
+            Console.WriteLine(status);
         }
 
         static void MinusNumbers(int a, int b)
@@ -22,6 +37,22 @@ namespace FirstApp9_3
         static void PlusNumbers(int a, int b)
         {
             Console.WriteLine(a + b);
+        }
+
+        static void ShowMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static int Sum(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        static bool CheckLength(string _row)
+        {
+            if (_row.Length > 3) return true;
+            return false;
         }
     }
 }
